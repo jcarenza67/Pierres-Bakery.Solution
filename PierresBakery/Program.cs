bfffc65b9bcc1f18a0e76cfe1dec46ef857e9bff
Joseph Wilfong <josephwilfong91@gmail.com>
2023-05-07 14:48:56 -0700

    add extra error handling

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -7,188 +7,98 @@ namespace PierresBakery
 {
   public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
-      Console.Title = "Pierre's Bakery";
-      string title = @" 
-                                                              (                                                                        
-        (  (           (                           *   )      )\ )                          (         (            )                   
-        )\))(   '   (  )\             )      (   ` )  /(     (()/( (     (   (    (      (  )\      ( )\     )  ( /(    (   (    (     
-        ((_)()\ )   ))\((_) (   (     (      ))\   ( )(_))(    /(_)))\   ))\  )(   )(    ))\((_)(    )((_) ( /(  )\())  ))\  )(   )\ ) 
-        _(())\_)() /((_)_   )\  )\    )\  ' /((_) (_(_()) )\  (_)) ((_) /((_)(()\ (()\  /((_)   )\  ((_)_  )(_))((_)\  /((_)(()\ (()/( 
-        \ \((_)/ /(_)) | | ((_)((_) _((_)) (_))   |_   _|((_) | _ \ (_)(_))   ((_) ((_)(_))    ((_)  | _ )((_)_ | |(_)(_))   ((_) )(_))
-         \ \/\/ / / -_)| |/ _|/ _ \| '  \()/ -_)    | | / _ \ |  _/ | |/ -_) | '_|| '_|/ -_)   (_-<  | _ \/ _` || / / / -_) | '_|| || |
-          \_/\_/  \___||_|\__|\___/|_|_|_| \___|    |_| \___/ |_|   |_|\___| |_|  |_|  \___|   /__/  |___/\__,_||_\_\ \___| |_|   \_, |
+      try
+      {
+        Console.Title = "Pierre's Bakery";
+        string title = @" 
+                                                                (                                                                        
+          (  (           (                           *   )      )\ )                          (         (            )                   
+          )\))(   '   (  )\             )      (   ` )  /(     (()/( (     (   (    (      (  )\      ( )\     )  ( /(    (   (    (     
+          ((_)()\ )   ))\((_) (   (     (      ))\   ( )(_))(    /(_)))\   ))\  )(   )(    ))\((_)(    )((_) ( /(  )\())  ))\  )(   )\ ) 
+          _(())\_)() /((_)_   )\  )\    )\  ' /((_) (_(_()) )\  (_)) ((_) /((_)(()\ (()\  /((_)   )\  ((_)_  )(_))((_)\  /((_)(()\ (()/( 
+          \ \((_)/ /(_)) | | ((_)((_) _((_)) (_))   |_   _|((_) | _ \ (_)(_))   ((_) ((_)(_))    ((_)  | _ )((_)_ | |(_)(_))   ((_) )(_))
+           \ \/\/ / / -_)| |/ _|/ _ \| '  \()/ -_)    | | / _ \ |  _/ | |/ -_) | '_|| '_|/ -_)   (_-<  | _ \/ _` || / / / -_) | '_|| || |
+            \_/\_/  \___||_|\__|\___/|_|_|_| \___|    |_| \___/ |_|   |_|\___| |_|  |_|  \___|   /__/  |___/\__,_||_\_\ \___| |_|   \_, |
                                                                                                                                   |__/  
-      ";
+        ";
 
-      Console.WriteLine(title);
-      Console.WriteLine("Breads are $5 each, and we are having a buy 2 get 1 free sale!");
-      Console.WriteLine("Pastries are $2 each, and we are having a buy 3 get 1 free sale!");
+        Console.WriteLine(title);
+        Console.WriteLine("Breads are $5 each, and we are having a buy 2 get 1 free sale!");
+        Console.WriteLine("Pastries are $2 each, and we are having a buy 3 get 1 free sale!");
 
 
-      Bread sourdough = new Bread("sourdough", 5);
-      Bread wheat = new Bread("wheat", 5);
-      Bread rye = new Bread("rye", 5);
+        Bread sourdough = new Bread("sourdough", 5);
+        Bread wheat = new Bread("wheat", 5);
+        Bread rye = new Bread("rye", 5);
 
-      Pastry croissant = new Pastry("croissant", 2);
-      Pastry danish = new Pastry("danish", 2);
-      Pastry bearclaw = new Pastry("bearclaw", 2);
+        Pastry croissant = new Pastry("croissant", 2);
+        Pastry danish = new Pastry("danish", 2);
+        Pastry bearclaw = new Pastry("bearclaw", 2);
 
-      string orderType = "";
-      bool validInputType = false;
+        string orderType = "";
+        bool validInputType = false;
 
-      while (!validInputType)
-      {
-        Console.WriteLine("Would you like bread, pastries, or both? (Enter 'bread', 'pastries', or 'both')");
-        orderType = Console.ReadLine().ToLower();
-        if (orderType == "bread" || orderType == "pastries" || orderType == "both")
+        while (!validInputType)
         {
-          validInputType = true;
-        }
-        else
-        {
-          Console.WriteLine("Invalid input. Please enter 'bread', 'pastries', or 'both'.");
-        }
-      }
-
-      int breadTotal = 0;
-      int pastryTotal = 0;
-
-      decimal fullTotal = 0;
-
-      if (orderType == "bread" || orderType == "both")
-      {
-        bool validInput = false;
-        int breadOrderAmount = 0;
-
-        while (!validInput)
-        {
-          Console.WriteLine("Which type of bread would you like? (Enter 'sourdough', 'wheat', or 'rye')");
-          string breadType = Console.ReadLine().ToLower();
-
-          Bread selectedBread = null;
-          switch (breadType)
+          Console.WriteLine("Would you like bread, pastries, or both? (Enter 'bread', 'pastries', or 'both')");
+          orderType = Console.ReadLine().ToLower();
+          if (orderType == "bread" || orderType == "pastries" || orderType == "both")
           {
-            case "sourdough":
-              selectedBread = sourdough;
-              break;
-            case "wheat":
-              selectedBread = wheat;
-              break;
-            case "rye":
-              selectedBread = rye;
-              break;
-            default:
-              Console.WriteLine("Invalid input. Please enter 'sourdough', 'wheat', or 'rye'.");
-              continue;
-          }
-
-          Console.WriteLine("How many loaves of " + selectedBread.BreadType + " bread would you like?");
-          string input = Console.ReadLine();
-
-          if (int.TryParse(input, out breadOrderAmount))
-          {
-            if (breadOrderAmount >= 0)
-            {
-              validInput = true;
-              breadTotal = selectedBread.BreadOrder(breadOrderAmount);
-            }
-            else
-            {
-              Console.WriteLine("Invalid input. Please enter a positive number.");
-            }
+            validInputType = true;
           }
           else
           {
-            Console.WriteLine("Invalid input. Please enter a number.");
+            Console.WriteLine("Invalid input. Please enter 'bread', 'pastries', or 'both'.");
           }
         }
-      }
 
-      if (orderType == "pastries" || orderType == "both")
-      {
-        bool validInput = false;
-        int pastryOrderAmount = 0;
-        while (!validInput)
+        int breadTotal = 0;
+        int pastryTotal = 0;
+
+        decimal fullTotal = 0;
+
+        if (orderType == "bread" || orderType == "both")
         {
-          Console.WriteLine("What type of pastry would you like? (Enter 'croissant', 'danish', or 'bearclaw')");
-          string pastryType = Console.ReadLine().ToLower();
+          bool validInput = false;
+          int breadOrderAmount = 0;
 
-          Pastry selectedPastry = null;
-
-          switch (pastryType)
+          while (!validInput)
           {
-            case "croissant":
-              selectedPastry = croissant;
-              break;
-            case "danish":
-              selectedPastry = danish;
-              break;
-            case "bearclaw":
-              selectedPastry = bearclaw;
-              break;
-            default:
-              Console.WriteLine("Invalid input. Please enter 'croissant', 'danish', or 'bearclaw'.");
-              continue;
-          }
+            Console.WriteLine("Which type of bread would you like? (Enter 'sourdough', 'wheat', or 'rye')");
+            string breadType = Console.ReadLine().ToLower();
 
-          Console.WriteLine("How many " + selectedPastry.PastryType + "'s would you like?");
-          string input = Console.ReadLine();
-
-          if (int.TryParse(input, out pastryOrderAmount))
-          {
-            if (pastryOrderAmount >= 0)
+            Bread selectedBread = null;
+            switch (breadType)
             {
-              validInput = true;
-              pastryTotal = selectedPastry.PastryOrder(pastryOrderAmount);
+              case "sourdough":
+                selectedBread = sourdough;
+                break;
+              case "wheat":
+                selectedBread = wheat;
+                break;
+              case "rye":
+                selectedBread = rye;
+                break;
+              default:
+                Console.WriteLine("Invalid input. Please enter 'sourdough', 'wheat', or 'rye'.");
+                continue;
             }
-            else
+
+            Console.WriteLine("How many loaves of " + selectedBread.BreadType + " bread would you like?");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out breadOrderAmount))
             {
-              Console.WriteLine("Invalid input. Please enter a positive number.");
-            } 
-          }
-          else
-          {
-            Console.WriteLine("Invalid input. Please enter a number.");
-          }
-        }
-      }
-
-      fullTotal = breadTotal + pastryTotal;
-
-      bool validInputTip = false;
-      decimal tipAmount = 0;
-      decimal fullTotalWithTip = fullTotal;
-
-      while (!validInputTip)
-      {
-        Console.WriteLine("Would you like to leave a tip? (Enter 'yes' or 'no')");
-        string tipInput = Console.ReadLine().ToLower();
-
-        if (tipInput == "yes")
-        {
-          bool validInputPercentage = false;
-          decimal percentage = 0;
-
-          while (!validInputPercentage)
-          {
-            Console.WriteLine("How much would you like to tip? (Enter a % between 0 and 100)");
-            string inputPercent = Console.ReadLine();
-
-            if (decimal.TryParse(inputPercent, out percentage))
-            {
-              if (percentage >= 0 && percentage <= 100)
+              if (breadOrderAmount >= 0)
               {
-                validInputPercentage = true;
-                fullTotal = breadTotal + pastryTotal;
-                Tip tip = new Tip(percentage, fullTotal);
-                tipAmount = tip.Amount;
-                fullTotalWithTip = Math.Round(fullTotal + tipAmount, 2);
+                validInput = true;
+                breadTotal = selectedBread.BreadOrder(breadOrderAmount);
               }
               else
               {
-                Console.WriteLine("Invalid input. Please enter a number between 0% and 100%.");
+                Console.WriteLine("Invalid input. Please enter a positive number.");
               }
             }
             else
@@ -196,38 +106,135 @@ namespace PierresBakery
               Console.WriteLine("Invalid input. Please enter a number.");
             }
           }
-
-          validInputTip = true;
-
         }
-        else if (tipInput == "no")
+
+        if (orderType == "pastries" || orderType == "both")
         {
-          validInputTip = true;
+          bool validInput = false;
+          int pastryOrderAmount = 0;
+          while (!validInput)
+          {
+            Console.WriteLine("What type of pastry would you like? (Enter 'croissant', 'danish', or 'bearclaw')");
+            string pastryType = Console.ReadLine().ToLower();
+
+            Pastry selectedPastry = null;
+
+            switch (pastryType)
+            {
+              case "croissant":
+                selectedPastry = croissant;
+                break;
+              case "danish":
+                selectedPastry = danish;
+                break;
+              case "bearclaw":
+                selectedPastry = bearclaw;
+                break;
+              default:
+                Console.WriteLine("Invalid input. Please enter 'croissant', 'danish', or 'bearclaw'.");
+                continue;
+            }
+
+            Console.WriteLine("How many " + selectedPastry.PastryType + "'s would you like?");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out pastryOrderAmount))
+            {
+              if (pastryOrderAmount >= 0)
+              {
+                validInput = true;
+                pastryTotal = selectedPastry.PastryOrder(pastryOrderAmount);
+              }
+              else
+              {
+                Console.WriteLine("Invalid input. Please enter a positive number.");
+              } 
+            }
+            else
+            {
+              Console.WriteLine("Invalid input. Please enter a number.");
+            }
+          }
+        }
+
+        fullTotal = breadTotal + pastryTotal;
+
+        bool validInputTip = false;
+        decimal tipAmount = 0;
+        decimal fullTotalWithTip = fullTotal;
+
+        while (!validInputTip)
+        {
+          Console.WriteLine("Would you like to leave a tip? (Enter 'yes' or 'no')");
+          string tipInput = Console.ReadLine().ToLower();
+
+          if (tipInput == "yes")
+          {
+            bool validInputPercentage = false;
+            decimal percentage = 0;
+
+            while (!validInputPercentage)
+            {
+              Console.WriteLine("How much would you like to tip? (Enter a % between 0 and 100)");
+              string inputPercent = Console.ReadLine();
+
+              if (decimal.TryParse(inputPercent, out percentage))
+              {
+                if (percentage >= 0 && percentage <= 100)
+                {
+                  validInputPercentage = true;
+                  fullTotal = breadTotal + pastryTotal;
+                  Tip tip = new Tip(percentage, fullTotal);
+                  tipAmount = tip.Amount;
+                  fullTotalWithTip = Math.Round(fullTotal + tipAmount, 2);
+                }
+                else
+                {
+                  Console.WriteLine("Invalid input. Please enter a number between 0% and 100%.");
+                }
+              }
+              else
+              {
+                Console.WriteLine("Invalid input. Please enter a number.");
+              }
+            }
+
+            validInputTip = true;
+
+          }
+          else if (tipInput == "no")
+          {
+            validInputTip = true;
+          }
+          else
+          {
+            Console.WriteLine("Invalid input. Please enter 'yes' or 'no'.");
+          }
+        }
+        
+
+        if (breadTotal > 0)
+        {
+          Console.WriteLine("Your bread total is $" + breadTotal + ".");
+        }
+
+        if (pastryTotal > 0)
+        {
+          Console.WriteLine("Your pastry total is $" + pastryTotal + ".");
+        }
+
+        if (tipAmount > 0)
+        {
+          Console.WriteLine("Your order total is $" + fullTotalWithTip + " with the $" + tipAmount + " tip included.");
         }
         else
         {
-          Console.WriteLine("Invalid input. Please enter 'yes' or 'no'.");
+        Console.WriteLine("Your order total is $" + fullTotal + ".");
         }
       }
-      
-
-      if (breadTotal > 0)
+      catch (Exception ex)
       {
-        Console.WriteLine("Your bread total is $" + breadTotal + ".");
-      }
-
-      if (pastryTotal > 0)
-      {
-        Console.WriteLine("Your pastry total is $" + pastryTotal + ".");
-      }
-
-      if (tipAmount > 0)
-      {
-        Console.WriteLine("Your order total is $" + fullTotalWithTip + " with the $" + tipAmount + " tip included.");
-      }
-      else
-      {
-      Console.WriteLine("Your order total is $" + fullTotal + ".");
+        Console.WriteLine("An unexpected error occured: " + ex.Message);
       }
     }
   }
